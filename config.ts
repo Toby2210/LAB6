a4dc4ad24686061da6c7718a6da29e8e30e83be2
Toby2210 <106552704+Toby2210@users.noreply.github.com>
2024-03-25 08:41:39 +0000

    LAB7a
    LAB7a

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  host: "satao.db.elephantsql.com",
+  port: 5432,
+  user: "pwmocbvp",
+  password: "9fJi8vd6cNl-FeVJ2eY6gsZ0v2FmFtvN",
+  database: "pwmocbvp",
+  connection_limit: 100
+}
--- routes/articles.ts
+++ routes/articles.ts
@@ -1,6 +1,6 @@
 import Router, { RouterContext } from "koa-router";
 import bodyParser from "koa-bodyparser";
-
+import * as model from '../models/articles';
 // Since we are handling articles use a URI that begins with an appropriate path
 const router = new Router({ prefix: '/api/v1/articles' });
 // Temporarily define some random articles in an array.
@@ -15,35 +15,37 @@ const articles = [
   },
   { title: 'smart campus', fullText: 'smart campus is coming to IVE' }
 ];
+
 // Now we define the handler functions
 const getAll = async (ctx: RouterContext, next: any) => {
-  // Use the response body to send the articles as JSON.
-  ctx.body = articles;
+  let articles = await model.getAll();
+  if (articles.length) {
+    ctx.body = articles;
+  } else {
+    ctx.body = {}
+  }
   await next();
 }
 const getById = async (ctx: RouterContext, next: any) => {
-  // Get the ID from the route parameters.
-  let id = +ctx.params.id
-  // If it exists then return the article as JSON.
-  // Otherwise return a 404 Not Found status code
-  if ((id < articles.length + 1) && (id > 0)) {
-    ctx.body = articles[id - 1];
+  let id = ctx.params.id;
+  let article = await model.getById(id);
+  if (article.length) {
+    ctx.body = article[0];
   } else {
     ctx.status = 404;
   }
   await next();
 }
 const createArticle = async (ctx: RouterContext, next: any) => {
-  // The body parser gives us access to the request body on ctx.request.body.
-  // Use this to extract the title and fullText we were sent.
-  let { title, fullText } = ctx.request.body;
-  // In turn, define a new article for addition to the array.
-  let newArticle = { title: title, fullText: fullText };
-  articles.push(newArticle);
-  // Finally send back appropriate JSON and status code.
-  // Once we move to a DB store, the newArticle sent back will now have its ID.
-  ctx.status = 201;
-  ctx.body = newArticle;
+  const body = ctx.request.body;
+  let result = await model.add(body);
+  if (result.status == 201) {
+    ctx.status = 201;
+    ctx.body = body;
+  } else {
+    ctx.status = 500;
+    ctx.body = { err: "insert data failed" };
+  }
   await next();
 }
 const updateArticle = async (ctx: RouterContext, next: any) => {
